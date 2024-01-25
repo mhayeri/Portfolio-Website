ab0ca3dcf43b9c35f68882582669fdbb563466b5
mhayeri <hayerimustafa@gmail.com>
2024-01-24 21:15:55 -0800

    Render Social Links component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
     return (
         <div className="App">
             <Navbar />
+            <Home />
+            <SocialLinks />
         </div>
     );
 }
