0d4c0956492ba07510d4df52bf5f5569c8623c5e
mhayeri <hayerimustafa@gmail.com>
2024-01-24 16:43:46 -0800

    added mobile functionality to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Links } from "../utils/Navbar/Links";
-// import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+    const [mobile, setIsMobile] = useState(false);
+
     return (
         <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
             <div>
@@ -18,6 +20,26 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
+
+            <div
+                onClick={() => setIsMobile(!mobile)}
+                className="cursor-pointer pr-4 z-10 text-gray-500  md:hidden"
+            >
+                {mobile ? <FaTimes size={30} /> : <FaBars size={30} />}
+            </div>
+
+            {mobile && (
+                <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800">
+                    {Links.map((link, index) => (
+                        <li
+                            key={index}
+                            className="px-4 cursor-pointer py-6 text-4xl"
+                        >
+                            {link}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
