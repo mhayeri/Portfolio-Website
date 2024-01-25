e5343105c62df64eb505b903c1737b5c06181b34
mhayeri <hayerimustafa@gmail.com>
2024-01-24 21:14:41 -0800

    added util for social media

---
+++ src/utils/Socials/SocialLinks.js
@@ -0,0 +1,25 @@
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+
+export const socials = [
+    {
+        id: 1,
+        social_media_name: "LinkedIn",
+        social_media_url: "https://www.linkedin.com/in/mustafa-hayeri/",
+        social_media_colorway: "bg-gradient-to-r from-cyan-500 to-blue-500",
+        social_media_icon: FaLinkedin,
+    },
+    {
+        id: 2,
+        social_media_name: "Github",
+        social_media_url: "https://github.com/mhayeri",
+        social_media_colorway: "bg-gradient-to-r from-green-400 to-green-600",
+        social_media_icon: FaGithub,
+    },
+    {
+        id: 3,
+        social_media_name: "Instagram",
+        social_media_url: "https://www.instagram.com/mustafahayeri/",
+        social_media_colorway: "bg-gradient-to-r from-purple-500 to-pink-500",
+        social_media_icon: FaInstagram,
+    },
+];
