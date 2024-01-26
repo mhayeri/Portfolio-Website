9d8ee5ef8319812d2d8f879d475a182c37c8c01e
mhayeri <hayerimustafa@gmail.com>
2024-01-25 19:35:55 -0800

    Added util for projects

---
+++ src/utils/Projects/Projects.js
@@ -0,0 +1,39 @@
+import Campfinder from "../../assets/images/CampFinder.png";
+import Netflix from "../../assets/images/Netflix.png";
+import Portfolio from "../../assets/images/Portfolio.png";
+import LeetCode from "../../assets/images/LeetCode.png";
+
+export const projects = [
+    {
+        id: 1,
+        project_image: Campfinder,
+        project_url: "https://github.com/mhayeri/CampFinder",
+        project_name: "CampFinder",
+        project_description:
+            "A full-stack application that replicates key features of Yelp. Allows users to create an account in order to discover, review, and manage campgrounds and reviews.",
+    },
+    {
+        id: 2,
+        project_image: Netflix,
+        project_url: "https://github.com/mhayeri/Netflix-Clone",
+        project_name: "Netflix Clone",
+        project_description:
+            "A Netflix clone built using React.js, Webhooks, CSS, Firestore, and Stripe Checkout. Movie data is fetched from the TMDB API.",
+    },
+    {
+        id: 3,
+        project_image: Portfolio,
+        project_url: "https://github.com/mhayeri/Portfolio-Website",
+        project_name: "Portfolio Website",
+        project_description:
+            "A portfolio website built using React.js and Tailwind CSS. Showcases my experience, skills and projects.",
+    },
+    {
+        id: 4,
+        project_image: LeetCode,
+        project_url: "https://github.com/mhayeri/Technical-Interview",
+        project_name: "Techincal-Interview",
+        project_description:
+            "Solutions to commonly asked LeetCode problems in Java. Tracking progress for technical interview preparation.",
+    },
+];
