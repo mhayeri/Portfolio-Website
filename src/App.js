9d43b444476e2f0dc69e194171d2b01e2501e3b2
mhayeri <hayerimustafa@gmail.com>
2024-01-25 21:24:03 -0800

    Added contact component to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <SocialLinks />
             <Skills />
             <Projects />
+            <Contact />
         </div>
     );
 }
