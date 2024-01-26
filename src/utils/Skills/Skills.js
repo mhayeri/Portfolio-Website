dcce7b8b39a31dea87a86649d3be49c89745370e
mhayeri <hayerimustafa@gmail.com>
2024-01-25 20:43:56 -0800

    Added util for skills

---
+++ src/utils/Skills/Skills.js
@@ -0,0 +1,150 @@
+import bootstrap from "../../assets/logos/bootstrap.png";
+import csharp from "../../assets/logos/c-sharp.png";
+import css from "../../assets/logos/css.png";
+import express from "../../assets/logos/express.png";
+import git from "../../assets/logos/git.png";
+import github from "../../assets/logos/github.png";
+import gitlab from "../../assets/logos/gitlab.png";
+import html from "../../assets/logos/html.png";
+import intellij from "../../assets/logos/intellij.png";
+import java from "../../assets/logos/java.png";
+import javascript from "../../assets/logos/javascript.png";
+import jenkins from "../../assets/logos/jenkins.png";
+import jira from "../../assets/logos/jira.png";
+import mysql from "../../assets/logos/mysql.png";
+import nodejs from "../../assets/logos/node-js.png";
+import postgresql from "../../assets/logos/postgresql.png";
+import pycharm from "../../assets/logos/pycharm.png";
+import python from "../../assets/logos/python.png";
+import react from "../../assets/logos/react.png";
+import tailwind from "../../assets/logos/tailwind.png";
+import vscode from "../../assets/logos/vs-code.png";
+
+export const skills = [
+    {
+        id: 1,
+        skill_logo: java,
+        skill_name: "Java",
+        skill_style: "shadow-blue-500",
+    },
+    {
+        id: 2,
+        skill_logo: python,
+        skill_name: "Python",
+        skill_style: "shadow-yellow-500",
+    },
+    {
+        id: 3,
+        skill_logo: csharp,
+        skill_name: "C#",
+        skill_style: "shadow-purple-500",
+    },
+    {
+        id: 4,
+        skill_logo: html,
+        skill_name: "HTML",
+        skill_style: "shadow-orange-500",
+    },
+    {
+        id: 5,
+        skill_logo: css,
+        skill_name: "CSS",
+        skill_style: "shadow-blue-500",
+    },
+    {
+        id: 6,
+        skill_logo: javascript,
+        skill_name: "JavaScript",
+        skill_style: "shadow-yellow-500",
+    },
+    {
+        id: 7,
+        skill_logo: react,
+        skill_name: "React.js",
+        skill_style: "shadow-blue-500",
+    },
+    {
+        id: 8,
+        skill_logo: nodejs,
+        skill_name: "Node.js",
+        skill_style: "shadow-green-500",
+    },
+    {
+        id: 9,
+        skill_logo: mysql,
+        skill_name: "MySQL",
+        skill_style: "shadow-blue-500",
+    },
+    {
+        id: 10,
+        skill_logo: postgresql,
+        skill_name: "PostgreSQL",
+        skill_style: "shadow-blue-500",
+    },
+    {
+        id: 11,
+        skill_logo: bootstrap,
+        skill_name: "Bootstrap",
+        skill_style: "shadow-purple-500",
+    },
+    {
+        id: 12,
+        skill_logo: express,
+        skill_name: "Express.js",
+        skill_style: "shadow-blue-500",
+    },
+    {
+        id: 13,
+        skill_logo: tailwind,
+        skill_name: "Tailwind CSS",
+        skill_style: "shadow-cyan-500",
+    },
+    {
+        id: 14,
+        skill_logo: git,
+        skill_name: "Git",
+        skill_style: "shadow-orange-500",
+    },
+    {
+        id: 15,
+        skill_logo: github,
+        skill_name: "GitHub",
+        skill_style: "shadow-gray-500",
+    },
+    {
+        id: 16,
+        skill_logo: gitlab,
+        skill_name: "GitLab",
+        skill_style: "shadow-orange-500",
+    },
+    {
+        id: 17,
+        skill_logo: jira,
+        skill_name: "Jira",
+        skill_style: "shadow-blue-500",
+    },
+    {
+        id: 18,
+        skill_logo: jenkins,
+        skill_name: "Jenkins",
+        skill_style: "shadow-blue-500",
+    },
+    {
+        id: 19,
+        skill_logo: intellij,
+        skill_name: "IntelliJ",
+        skill_style: "shadow-red-500",
+    },
+    {
+        id: 20,
+        skill_logo: pycharm,
+        skill_name: "PyCharm",
+        skill_style: "shadow-green-500",
+    },
+    {
+        id: 21,
+        skill_logo: vscode,
+        skill_name: "Visual Studio Code",
+        skill_style: "shadow-blue-500",
+    },
+];
