5376f858957297bb39153018f7b918898238d33a
mhayeri <hayerimustafa@gmail.com>
2024-01-26 11:22:11 -0800

    Updated project descriptions

--- src/utils/Projects/Projects.js
+++ src/utils/Projects/Projects.js
@@ -18,7 +18,7 @@ export const projects = [
         project_url: "https://github.com/mhayeri/Netflix-Clone",
         project_name: "Netflix Clone",
         project_description:
-            "A Netflix clone built using React.js, Webhooks, CSS, Firestore, and Stripe Checkout. Movie data is fetched from the TMDB API.",
+            "A Netflix clone built using React.js, Webhooks, CSS, Firestore, and Stripe Checkout. Movie data is fetched from the TMDb API.",
     },
     {
         id: 3,
@@ -26,7 +26,7 @@ export const projects = [
         project_url: "https://github.com/mhayeri/Portfolio-Website",
         project_name: "Portfolio Website",
         project_description:
-            "A portfolio website built using React.js and Tailwind CSS. Showcases my experience, skills and projects.",
+            "A portfolio website built using React.js and Tailwind CSS that showcases my work experience, technical skills and projects.",
     },
     {
         id: 4,
