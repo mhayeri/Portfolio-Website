0b27f42980ec7ffc7c8a29a4ed3f15454190078c
mhayeri <hayerimustafa@gmail.com>
2024-01-27 11:04:46 -0800

    Adjusting button sizing/layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
             name="HOME"
             className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
         >
-            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-                <div className="flex flex-col justify-center my-20">
+            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-screen px-4 md:flex-row ">
+                <div className="flex flex-col justify-center my-10 sm:my-20 px-4 mt-32 md:mt-0">
                     <h2 className="text-4xl sm:text-6xl font-bold text-white">
                         Software Engineer
                     </h2>
@@ -22,14 +22,14 @@ const Home = () => {
                         lifecycle (Agile). Bachelor of Science (B.S.) in
                         Computer Science from San Diego State University.
                     </p>
-                    <div className="flex flex-row">
+                    <div className="flex flex-col md:flex-row md:justify-between md:space-x-3">
                         <a
                             href="/MustafaResume.pdf"
                             download="Mustafa_Hayeri_Resume.pdf"
                         >
-                            <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-purple-500 to-pink-500 cursor-pointer hover:scale-105 duration-300">
+                            <button className="text-white w-full md:w-auto px-6 py-3 my-2 flex justify-center items-center rounded-md bg-gradient-to-r from-purple-500 to-pink-500 cursor-pointer hover:scale-105 duration-300">
                                 Resume
-                                <span>
+                                <span className="ml-2 hidden md:inline-block">
                                     <FaFileDownload className="ml-1" />
                                 </span>
                             </button>
@@ -38,11 +38,10 @@ const Home = () => {
                             href="https://www.linkedin.com/in/mustafa-hayeri/"
                             target="_blank"
                             rel="noreferrer"
-                            className="mx-4"
                         >
-                            <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:scale-105 duration-300">
+                            <button className="text-white w-full md:w-auto px-6 py-3 my-2 flex justify-center items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:scale-105 duration-300">
                                 LinkedIn
-                                <span>
+                                <span className="ml-2 hidden md:inline-block">
                                     <FaLinkedin className="ml-1" />
                                 </span>
                             </button>
@@ -52,9 +51,9 @@ const Home = () => {
                             target="_blank"
                             rel="noreferrer"
                         >
-                            <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-green-400 to-green-600 cursor-pointer hover:scale-105 duration-300">
+                            <button className="text-white w-full md:w-auto px-6 py-3 my-2 flex justify-center items-center rounded-md bg-gradient-to-r from-green-400 to-green-600 cursor-pointer hover:scale-105 duration-300">
                                 Github
-                                <span>
+                                <span className="ml-2 hidden md:inline-block">
                                     <FaGithub className="ml-1" />
                                 </span>
                             </button>
@@ -62,7 +61,7 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className="w-60 h-60 md:w-72 md:h-72 mx-auto overflow-hidden">
+                <div className="w-48 h-48 md:w-72 md:h-72 mx-auto overflow-hidden">
                     <img src={profile} alt="profile" />
                 </div>
             </div>
